55c39e960cf97162872cc1ef43625de7ee23ddbc
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-03-11 12:08:11 +0530

    Create UserDto

--- src/main/java/lk/ijse/dto/UserDto.java
+++ src/main/java/lk/ijse/dto/UserDto.java
@@ -1,4 +1,71 @@
 package lk.ijse.dto;
 
 public class UserDto {
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public UserDto() {
+    }
+
+    public UserDto(int userId, String firstName, String lastName, String email, String password) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
