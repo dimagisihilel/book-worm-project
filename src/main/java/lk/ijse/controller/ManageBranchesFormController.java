556643b761f481d65fcb07805709f054b36bf6c5
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-03-12 18:21:29 +0530

    Add IDs to branch form

--- src/main/java/lk/ijse/controller/ManageBranchesFormController.java
+++ src/main/java/lk/ijse/controller/ManageBranchesFormController.java
@@ -1,5 +1,42 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 public class ManageBranchesFormController {
 
+
+    public TableView tblbranches;
+    public TableColumn colBranchName;
+    public TableColumn colBranchAddress;
+    public TableColumn colBranchContact;
+    public TextField txtBranchName;
+    public TextField txtBranchContact;
+    public TextField txtBranchAddress;
+    public Button btnAddbranch;
+    public Button btnUpdatebranch;
+    public Button btnDltbranch;
+
+    public void btnAddbranchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdatebranchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDltbranchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtBranchNameOnKeyReleased(KeyEvent keyEvent) {
+    }
+
+    public void txtBranchContactOnKeyReleased(KeyEvent keyEvent) {
+    }
+
+    public void txtBranchAddressOnKeyReleased(KeyEvent keyEvent) {
+
+    }
 }
