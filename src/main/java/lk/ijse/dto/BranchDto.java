c9c2b0afd9d65b9e809cbada663f52def6ff5f04
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-03-12 18:43:02 +0530

    Update the branchDto

--- src/main/java/lk/ijse/dto/BranchDto.java
+++ src/main/java/lk/ijse/dto/BranchDto.java
@@ -1,4 +1,71 @@
 package lk.ijse.dto;
 
 public class BranchDto {
+    private int branchId;
+    private String branchName;
+    private String branchAddress;
+    private String branchContact;
+    private int adminId;
+
+    public BranchDto() {
+    }
+
+    public BranchDto(int branchId, String branchName, String branchAddress, String branchContact, int adminId) {
+        this.branchId = branchId;
+        this.branchName = branchName;
+        this.branchAddress = branchAddress;
+        this.branchContact = branchContact;
+        this.adminId = adminId;
+    }
+
+    public int getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(int branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getBranchAddress() {
+        return branchAddress;
+    }
+
+    public void setBranchAddress(String branchAddress) {
+        this.branchAddress = branchAddress;
+    }
+
+    public String getBranchContact() {
+        return branchContact;
+    }
+
+    public void setBranchContact(String branchContact) {
+        this.branchContact = branchContact;
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    @Override
+    public String toString() {
+        return "BranchDto{" +
+                "branchId=" + branchId +
+                ", branchName='" + branchName + '\'' +
+                ", branchAddress='" + branchAddress + '\'' +
+                ", branchContact='" + branchContact + '\'' +
+                ", adminId=" + adminId +
+                '}';
+    }
 }
